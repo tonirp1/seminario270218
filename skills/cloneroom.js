5dc1a8fa8c77fadab33c207ad7152903e60f1f0e
tonirp <arodriguez@makenai.es>
2018-02-27 15:38:07 +0100

    final

--- skills/cloneroom.js
+++ skills/cloneroom.js
@@ -1,37 +1,34 @@
-//
-// Command: help
-//
 module.exports = function (controller) {
-
-    controller.hears(["room"], 'direct_message,direct_mention', function (bot, message) {
-        var email = message.user;
-        var CiscoSpark = require('node-ciscospark');
-        var async = require('async');
-        var spark = new CiscoSpark(process.env.SPARK_TOKEN);
-        bot.startConversation(message, function (err, convo) {
-            var question = "What name do you want for the new room?";
-            convo.ask(question, [
-              {
-                    default: true,
-                    callback: function (response, convo) {
-                        var membershipParameters = {"roomId": message.channel };
-                        var roomName = response.text;
-                        convo.say("Creating room " + "**"+response.text +"**...");
+controller.hears(["room"], 'direct_message,direct_mention', function (bot, message) {
+            var email = message.user;
+            var CiscoSpark = require('node-ciscospark');
+            var async = require('async');
+            var spark = new CiscoSpark(process.env.SPARK_TOKEN);
+    
+            bot.startConversation(message, function (err, convo) {
+                var question = "What name do you want for the new room?";
+                convo.ask(question, [
+                  {
+                        default: true,
+                        callback: function (response, convo) {
+                            var membershipParameters = {"roomId": message.channel };
+                            var roomName = response.text;
+                            convo.say("Creating room " + "**"+response.text +"**...");
                         async.waterfall([
-                            function (callback){
-                                     spark.rooms.create({"title": roomName}, function(err, response) {
-                                         if(err)
-                                         {
-                                             convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
-                                             convo.next(); 
-                                           }
-                                         else{
-                                       var responseObj = JSON.parse(response);
-                                       callback(null, responseObj.id);
-                                         }
-                                   });
+                               function (callback){
+                                  spark.rooms.create({"title": roomName}, function(err, response) {
+                                      if(err)
+                                      {
+                                          convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
+                                          convo.next(); 
+                                        }
+                                      else{
+                                    var responseObj = JSON.parse(response);
+                                    callback(null, responseObj.id);
+                                      }
+                                });
                                 },
-                                function (roomId, callback){
+                              function (roomId, callback){
                                     spark.memberships.list(membershipParameters, function(err, response) {
                                         if(err)
                                         {
@@ -43,21 +40,21 @@ module.exports = function (controller) {
                                       }
                                 });
                                 },
-                                function (response, roomId, callback){
-                                    var responseObj = JSON.parse(response);
-                                    if(responseObj.items.length > 0){
-                                      responseObj.items.forEach(function(element) {
-                                        var param = {"teamId": roomId, "roomId" : roomId, "personEmail" : element.personEmail, "isModerator" : element.isModerator };
-                                        spark.memberships.create(param, function(err, response){
-                                           console.log(err);   });
-                                       });
-                                       convo.say("I have created the room. You can check it now.");
-                                       convo.next();
-                                    }
-                                    else{
-                                       convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
-                                       convo.next();
-                                    }
+                              function (response, roomId, callback){
+                                 var responseObj = JSON.parse(response);
+                                 if(responseObj.items.length > 0){
+                                   responseObj.items.forEach(function(element) {
+                                     var param = {"teamId": roomId, "roomId" : roomId, "personEmail" : element.personEmail, "isModerator" : element.isModerator };
+                                     spark.memberships.create(param, function(err, response){
+                                        console.log(err);   });
+                                    });
+                                    convo.say("I have created the room. You can check it now.");
+                                    convo.next();
+                                 }
+                                 else{
+                                    convo.say("I am sorry, there was an error creating the room, Please contact administrator.");
+                                    convo.next();
+                                  }
                                 }
                             ]);
                     }
@@ -66,7 +63,3 @@ module.exports = function (controller) {
             });
         });
     }
-    
-
-    });
-}
